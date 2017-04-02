f488fcc9cece529cf1c58589ee7278a1390de1a5
David <david@ijustwritecode.com>
2017-04-02 10:44:14 +0100

    Add parcellite config

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-# Symlink dotfiles. 
+# Symlink dotfiles.
 echo "Linking dotfiles..."
-FILES=".oh-my-zsh/custom/ssh-add.zsh .config/dunst/dunstrc .xinitrc .Xdefaults .vimrc .zlogin .zshrc .conkyrc .tmux.conf"
+FILES=".oh-my-zsh/custom/ssh-add.zsh .config/dunst/dunstrc .config/parcellite/parcelliterc .xinitrc .Xdefaults .vimrc .zlogin .zshrc .conkyrc .tmux.conf"
 for F in $FILES; do
 	if [ -e ~/$F ]; then
 		echo ~/$F exists - backup created
-		# Create dated backup. 
+		# Create dated backup.
 		mv ~/$F ~/$F.$(date +%Y%m%d)
 	else
 		mkdir -p $(dirname ~/$F)
